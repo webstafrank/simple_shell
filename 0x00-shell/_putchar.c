22f90375f1f799c9fc86e486b6cfee43e90ac132
webstafrank <franklyndenish@gmail.com>
2024-02-23 23:57:56 -0800

    header and a readme

---
+++ 0x00-shell/_putchar.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/* _putchar prints to thr stdout
+ * prototype - void print_char(char c)
+ * returns 1 0n success and 0 on fail 
+ */
+char print_char(char c)
+{
+	return(write (1, &c, 1))
+}
---
+++ 0x00-shell/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unstd.h>
+#include <stdib.h>
+#include <stdarg.h>
+
+void print_char(char c);
+
+#endif
